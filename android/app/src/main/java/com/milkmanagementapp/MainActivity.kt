619b1467dc2d16e1255b1e27179a7517e7b6f4fa
AndroidVersatile <app.vpipl@gmail.com>
2026-01-20 16:13:47 +0530

    android completed

--- android/app/src/main/java/com/milkmanagementapp/MainActivity.kt
+++ android/app/src/main/java/com/milkmanagementapp/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.milkmanagementapp
-
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
-
+import com.zoontek.rnbootsplash.RNBootSplash
 class MainActivity : ReactActivity() {
 
   /**
@@ -12,7 +12,10 @@ class MainActivity : ReactActivity() {
    * rendering of the component.
    */
   override fun getMainComponentName(): String = "MilkManagementApp"
-
+   override fun onCreate(savedInstanceState: Bundle?) {
+    RNBootSplash.init(this, R.style.BootTheme) // ⬅️ initialize the splash screen
+    super.onCreate(savedInstanceState) // super.onCreate(null) with react-native-screens
+  }
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
